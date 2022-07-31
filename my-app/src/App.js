cddeebe8a58e00e69d272b8a6961d9ff64e52d93
Naman Ajmera <cool.naman.ajmera@gmail.com>
2022-07-31 11:40:13 +0530

    Added: Add the About Us using UseState.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import NavBar from "./commonComponents/NavBar";
-import TextForm from './forms/TextForm';
+import About from './forms/About';
+// import TextForm from './forms/TextForm';
 
 function App() {
-  const heading = "Enter the text to analyze";
+  // const heading = "Enter the text to analyze";
   return (
     <div>
       <NavBar title="Text Utils" />
       <div className="container my-3">
-        <TextForm heading={heading} />
+        {/* <TextForm heading={heading} /> */}
+        <About />
       </div>
     </div>
   );
--- my-app/src/commonComponents/NavBar.jsx
+++ my-app/src/commonComponents/NavBar.jsx
@@ -17,18 +17,7 @@ export default function NavBar(props) {
                         <a className="nav-link active" aria-current="page" href="/">Home</a>
                      </li>
                      <li className="nav-item">
-                        <a className="nav-link" href="/">Link</a>
-                     </li>
-                     <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                           Dropdown
-                        </a>
-                        <ul className="dropdown-menu">
-                           <li><a className="dropdown-item" href="/">Action</a></li>
-                           <li><a className="dropdown-item" href="/">Another action</a></li>
-                           <li><hr className="dropdown-divider" /></li>
-                           <li><a className="dropdown-item" href="/">Something else here</a></li>
-                        </ul>
+                        <a className="nav-link" aria-current="page" href="/">About</a>
                      </li>
                      <li className="nav-item">
                         <a className="nav-link disabled" href="/">{props.about}</a>
