7e6c72c3abfc37e3522a5bde41718e40f7cee68d
Naman Ajmera <naman.ajmera@magicedtech.com>
2022-08-03 12:17:24 +0530

    Added: Change the port.

--- mernProject/backend/index.js
+++ mernProject/backend/index.js
@@ -4,13 +4,13 @@ const express = require('express')
 connectToMongo();
 
 const app = express()
-const port = 3000
+const port = 5000
 
 // Middle Ware to use JSON as a body in API
 app.use(express.json());
 
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/notes',require('./routes/notes'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`)
