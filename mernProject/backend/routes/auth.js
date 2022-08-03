56762e5ed41f0fdbf4cac2847e6d4a0b1c396c22
Naman Ajmera <naman.ajmera@magicedtech.com>
2022-08-03 21:21:39 +0530

    Added: setup jwt.

--- mernProject/backend/routes/auth.js
+++ mernProject/backend/routes/auth.js
@@ -2,21 +2,24 @@ const express = require('express');
 const Users = require('../models/Users');
 const router = express.Router();
 const {body, validationResult} = require('express-validator')
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.post('/createUser',[
    body('email','Please enter a valid Email').isEmail(),
    body('password','Minimum Lenght should be 8').isLength({min:8}),
    body('name','PLease enter a valid Name').isLength({min:5})
-],(req,res) => {
+],async (req,res) => {
    const errors = validationResult(req);
    if (!errors.isEmpty()) {
       return res.status(400).json({errors: errors.array()});
    }
+   const salt = await bcrypt.genSalt(10);
+   const secPass = await bcrypt.hash(req.body.password,salt)
    Users.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: secPass,
    }).then(user => res.json(user)).catch(error => {
       console.log(error);
       res.json({error:"Please enter a unique value for email.",message:error.message})
