12b31d5278201510ad1f35e2221f2e0e702eee63
Naman Ajmera <cool.naman.ajmera@gmail.com>
2022-08-06 22:00:10 +0530

    Added: Setup the basic structure.

---
+++ reduxapp/src/Shop.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div className='container my-5 d-flex align-items-center'>
+      <button className="btn btn-primary mx-3">-</button>
+      <div>Add to cart</div>
+      <button className="btn btn-primary mx-3">+</button>
+    </div>
+  )
+}
+
+export default Shop
