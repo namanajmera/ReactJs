db2b5e246ed3c9803f459944f95c1d6051ed162f
Naman Ajmera <cool.naman.ajmera@gmail.com>
2022-08-06 11:29:05 +0530

    Adding some icon for performnce.

--- mernProject/inotebook/src/components/NotesItem.jsx
+++ mernProject/inotebook/src/components/NotesItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// import { trash } from '../assets'
 
 export default function NotesItem(props) {
    const { title, description, tag } = props.noteItem;
@@ -7,7 +8,11 @@ export default function NotesItem(props) {
       <div className='col-md-3'>
          <div className="card my-3">
             <div className="card-body">
-               <h5 className="card-title">{title}</h5>
+               <div className="d-flex justify-content align-items">
+                  <h5 className="card-title">{title}</h5>
+                  <img className='mx-2' src="https://cdn-icons-png.flaticon.com/512/3096/3096673.png" alt="" style={{ width: "25px", height: "25px", cursor: "pointer" }} />
+                  <img className='mx-2' src="https://as1.ftcdn.net/v2/jpg/03/73/50/00/1000_F_373500029_TSJ1vErHtB2nUtHoAtaFAwe47fqSJbEd.jpg" alt="" style={{ width: "25px", height: "25px", cursor: "pointer" }} />
+               </div>
                <h6 className="card-subtitle mb-2 text-muted">{tag}</h6>
                <p className="card-text">{description}</p>
             </div>
