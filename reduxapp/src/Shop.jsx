2cff9fb752b9f7c7b5888c42a4b77286b61bac58
Naman Ajmera <cool.naman.ajmera@gmail.com>
2022-08-07 12:22:32 +0530

    Added: Structure of Actions and Reducers.

--- reduxapp/src/Shop.jsx
+++ reduxapp/src/Shop.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 function Shop() {
   return (
-    <div className='container my-5 d-flex align-items-center'>
-      <button className="btn btn-primary mx-3">-</button>
-      <div>Add to cart</div>
-      <button className="btn btn-primary mx-3">+</button>
+    <div className='container my-5'>
+      <h2>Deposit/Withdraw Money</h2><br />
+      <div className='d-flex align-items-center'>
+        <button className="btn btn-primary mx-3">-</button>
+        <div>Update Balance</div>
+        <button className="btn btn-primary mx-3">+</button>
+      </div>
     </div>
   )
 }
